9b711e78dfdb12e028a6d17f57bef7c89d5e4658
Da2022-05 <avilapedrozad@gmail.com>
2021-06-02 18:31:21 -0500

    Numero par o impar

---
+++ Ejercicio/mostrar.php
@@ -0,0 +1,15 @@
+<?php 
+    if(isset($_POST['numero'])){
+        $num=intval($_POST['numero']);
+        if ($num!== ""){
+            $value = "";
+        } 
+        if ($num%2==0){ 
+            $value = 1;
+        } else {
+            $value = 0;
+        }
+    }
+    header("Location: ./pareimpar.php?confirm=".$value);
+    ?>
+
